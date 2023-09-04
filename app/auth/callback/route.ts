e49ccc4fdf7e83cd320365cb58eb2ddb979fe1d4
Mykyta Holtsov <nikitaholtsov@gmail.com>
2023-09-04 11:31:17 +0300

    ready

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -4,9 +4,6 @@ import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-
-const supabase = createServerComponentClient({ cookies });
 
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url);
